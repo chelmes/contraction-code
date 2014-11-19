cbde4b3c5546bea0cfa13fcc7ba4130061159eab
Christopher Helmes <helmes@hiskp.uni-bonn.de>
2014-11-19 15:30:54 +0100

    nicer output for demonstration

--- main/test_gaugefield.cpp
+++ main/test_gaugefield.cpp
@@ -1,3 +1,10 @@
+/***************************************************************************/
+//
+// Short demonstration of gaugefield class
+//
+// Author: Christopher Helmes
+//
+/***************************************************************************/
 #include <iostream>
 #include <iomanip>
 #include <complex>
@@ -20,16 +27,23 @@ int main(int ac, char* av[]) {
   //read in eigenvectors
   evec.read_eigen_vector("/hiskp2/eigensystems/test4x4x4x4/eigenvectors.1000.",1);
   //initialize lookup table
+  std::cout << "\ninitialize lookup tables:" << std::endl;
   gf.init(4,4,4);
   //read in gaugefield
+  std::cout << "\nRead in complete 4^4 configuration:" << std::endl;
   gf.read_gauge_field(0,3);
+  std::cout << "\n U at t = 3, vol = 4, dir = z:" << std::endl;
   std::cout << gf(3,4,2) << std::endl;
   //smear gauge field
   //gf.smearing_stout(3,0.62,3);
-  std::cout << gf.plaque_ts(0) << std::endl;
+  std::cout << "\nPlaquette value at timeslice 0: " << 
+    gf.plaque_ts(0) << std::endl;
+  // trafo all timeslices
+  std::cout << "\nGauge transformation of whole confiugration" << std::endl;
   gf.trafo(0,3);
-  std::cout << gf.plaque_ts(0) << std::endl;
-  Eigen::MatrixXcd evec_t = gf.trafo_ev(evec[0]); 
+  std::cout << "\nPlaquette value at timeslice 0 after gauge_config:" <<
+    gf.plaque_ts(0) << std::endl;
+  Eigen::MatrixXcd evec_t = gf.trafo_ev(evec[0]);
   std::cout << (evec[0] * evec[0].adjoint()).trace() << std::endl;
   //displace eigensystem
   Eigen::MatrixXcd W = Eigen::MatrixXcd::Zero(4*4*4*3,4);
